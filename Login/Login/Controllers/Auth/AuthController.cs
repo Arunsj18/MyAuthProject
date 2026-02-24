99cffb62b7a083ba71166fea9a758e91a0a5f4ec
Arunsj18 <arun.s@saneforce.io>
2026-02-24 22:17:41 +0530

    Created controller

---
+++ Login/Login/Controllers/Auth/AuthController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Login.Controllers.Auth
+{
+    public class AuthController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
